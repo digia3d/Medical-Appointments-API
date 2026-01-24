6c4de3ff967b5b9300cbf548f72e8706563bbb70
Aleksandra <ujvari65@gmail.com>
2026-01-24 17:49:04 +0100

    Add User and Doctor models with associations and validations

---
+++ app/models/doctor.rb
@@ -0,0 +1,7 @@
+class Doctor < ApplicationRecord
+  belongs_to :user
+  has_many :appointments, dependent: :destroy
+
+  validates :name, presence: true
+  validates :specialization, presence: true
+end
