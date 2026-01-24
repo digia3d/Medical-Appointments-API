8b737a33f916ef03192d3678d8727787500fc09d
Aleksandra <ujvari65@gmail.com>
2026-01-24 19:59:03 +0100

    Fix linters error

--- db/schema.rb
+++ db/schema.rb
@@ -7,6 +7,8 @@
 # be faster and is potentially less error prone than running all of your
 # migrations from scratch. Old migrations may fail to apply correctly if those
 # migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2026_01_24_165225) do
   # These are extensions that must be enabled in order to support this database
